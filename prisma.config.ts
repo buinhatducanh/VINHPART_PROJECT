387d5c31fea2360f381155c918e54d716a5e5bd7
Bùi Nhật Đức Anh <124678780+buinhatducanh@users.noreply.github.com>
2026-03-03 23:17:10 +0700

    update

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,10 @@
-import path from 'node:path';
-import { defineConfig, env } from '@prisma/config';
+import 'dotenv/config';
+import path from 'path';
+import { defineConfig } from '@prisma/config';
 
 export default defineConfig({
     schema: path.join('backend', 'prisma', 'schema.prisma'),
     datasource: {
-        url: env('DATABASE_URL'),
+        url: process.env.DATABASE_URL!,
     },
 });
